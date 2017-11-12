929e5a21072f780938a0e559d2e2eafffebc36a2
unknown <andrewseward93@gmail.com>
2017-11-12 12:25:54 -0700

    Init. Check your @pima.edu email.

---
+++ Assets/DiscordChatGame/Scripts/Logging/Log.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class Log
+{
+    public static string Timestamp()
+    {
+        return $"[{System.DateTime.Now:HH:mm:ss:FFF tt}] ";
+    }
+
+    public static string ShortTime()
+    {
+        return $"[{System.DateTime.Now:mm:ss:FFF t}]";
+    }
+}
