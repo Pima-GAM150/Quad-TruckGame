71eaf9b9cb01a3cf9a8192e99e51cef5e670d578
mjackson34 <mjackson34@mail.pima.edu>
2017-11-30 19:28:53 -0700

    Trailer testing
    Have a basic trailer with one wall.  Need to duplicate the wall and make
    he trailer longer.

---
+++ Assets/DiscordChatGame/Scripts/CheckHingeJoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckHingeJoint : MonoBehaviour {
+
+    public HingeJoint hj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnJointBreak(float breakforce)
+    {
+        Debug.Log("Joint broke!");
+        hj.gameObject.transform.parent = null;
+    }
+}
